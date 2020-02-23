b65e8a7f39933a5bc3d62d2fc908eba5c573c4fa
MKachi <mkachi@naver.com>
2020-02-24 01:04:28 +0900

    정보 업데이트 주기 변경, 크롤링 방식 변경

--- app.js
+++ app.js
@@ -15,22 +15,26 @@ const sendNews = async () => {
   if (await crawler.update()) {
     app.guilds.forEach((guild) => {
       guild.channels.forEach((channel) => {
-        if (channel.type == 'text') {
-          const textChannel = new discord.TextChannel(guild, channel)
+        try {
+          if (channel.type == 'text') {
+            const textChannel = new discord.TextChannel(guild, channel)
 
-          const news = crawler.getLastInfo()
-          const embed = new discord.RichEmbed()
-            .setColor(0xFF0000)
-            .setThumbnail('https://i.imgur.com/dthfKX4.png')
-            .setTitle(news.getLabel())
-            .setDescription(news.getMessage())
-            .setFooter(news.getTimestamp())
-          textChannel.sendEmbed(embed)
+            const news = crawler.getLastInfo()
+            const embed = new discord.RichEmbed()
+              .setColor(0xFF0000)
+              .setThumbnail('https://i.imgur.com/dthfKX4.png')
+              .setTitle(news.getLabel())
+              .setDescription(news.getMessage())
+              .setFooter(news.getTimestamp())
+            textChannel.sendEmbed(embed)
+          }
+        } catch (except) {
+          writeLog('Except', except)
         }
       })
     })
   }
-  setTimeout(sendNews, 60000)
+  setTimeout(sendNews, 65000)
 }
 
 app.on('message', msg => router(crawler, msg))
@@ -38,7 +42,7 @@ app.on('ready', () => {
   writeLog('Info', `Corona bot is running at ${app.user.tag}`)
   crawler.update().then(() => {
     writeLog('Info', 'Crawler is ready!')
-    setTimeout(sendNews, 60000)
+    setTimeout(sendNews, 65000)
   })
 })
 app.login(process.env.TOKEN)
--- crawler/index.js
+++ crawler/index.js
@@ -23,29 +23,54 @@ module.exports = class Crawler {
   }
 
   async update() {
-    writeLog('Info', 'Update')
+    writeLog('Info', 'Fetch Data...')
     let flag = false
     let driver = await new webDriver.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build()
     try {
       await driver.get(url)
       await this.wait(3000)
+
+      const status = await driver.findElements(By.css('.stats-container > div > div > span'))
+      this._status = []
+      for (let i = 0; i < status.length; ++i) {
+        this._status.push(await status[i].getText())
+      }
+      writeLog('Data', `의심환자 : ${this._status[0]}명`)
+      writeLog('Data', `검사중 : ${this._status[1]}명`)
+      writeLog('Data', `음성판정 : ${this._status[2]}명`)
+      writeLog('Data', `확진자 : ${this._status[3]}명`)
+      writeLog('Data', `사망 : ${this._status[4]}명`)
+      writeLog('Data', `완치 : ${this._status[5]}명`)
+
       const messages = await driver.findElements(By.className('message'))
       for (let i = 0; i < messages.length; ++i) {
         const target = messages[i]
 
         // Label
-        const header = await target.findElements(By.css('.time > div'))
-        const label = await header[0].getText()
+        const label = await target.findElements(By.css('.time > .news'))
+        let labelText = ''
+        if (label.length > 0) {
+          labelText = await label.getText()
+        }
 
         // Timestamp
+        let timeInfos = await target.findElements(By.css('.time > div'))
         let timestamp = ''
-        if (header[1] != undefined && header.length > 1) {
-          timestamp = await header[1].getText()
+        if (timeInfos.length > 1) {
+          timestamp = await timeInfos[1].getText()
+        } else {
+          const check = await target.findElements(By.css('.news'))
+          if (check.length == 0) {
+            timestamp = await timeInfos[0].getText()
+          }
         }
 
         // 내용
-        const info = await target.findElement(By.css('.text > .info'))
-        const description = await info.getText()
+        const info = await target.findElements(By.css('.text > .info'))
+        let description = ''
+        if (info.length > 0) {
+          description = await info[0].getText()
+        }
 
         // 출처
         const source = await target.findElements(By.css('.source > a'))
@@ -55,31 +80,31 @@ module.exports = class Crawler {
         }
 
         if (i == 0) {
-          const last = new News(label, timestamp, description, link)
+          const last = new News(labelText, timestamp, description, link)
           if (this._lastMessage != null && this._lastMessage.equals(last)) {
+            writeLog('Info', 'Already up to date')
             return flag
           }
           flag = true
           this._lastMessage = last
+          writeLog('Data', 'News')
+          writeLog('Data', `[${labelText}] ${timestamp}`)
+          writeLog('Data', description)
+          writeLog('Data', link)
         }
 
-        if (label != '개발자') {
-          this._lastInfo = new News(label, timestamp, description, link)
+        if (labelText != '개발자') {
+          this._lastInfo = new News(labelText, timestamp, description, link)
           break;
         }
       }
-
-      const status = await driver.findElements(By.css('.stats-container > div > div > span'))
-      this._status = []
-      for (let i = 0; i < status.length; ++i) {
-        this._status.push(await status[i].getText())
-      }
     } catch (except) {
       writeLog('Except', except)
-      return flag
+      return false
 
     } finally {
       driver.quit()
+      writeLog('Info', 'Finish!')
     }
     return flag
   }
